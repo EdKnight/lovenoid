f6bbbc6f1df1906779b843cbeedc2b1f3c32e93e
Eduardo <edusb7@gmail.com>
2017-09-23 12:53:12 -0300

    Images
    Added images.
    Platform and ball with images

--- src/ball.lua
+++ src/ball.lua
@@ -3,9 +3,17 @@ local ball = {}
 --object properties
 ball.position_x = 300
 ball.position_y = 400
+ball.image = love.graphics.newImage("img/ball.png")
+ball.x_tile_pos = 0
+ball.y_tile_pos = 0
+ball.tile_width = 18
+ball.tile_height = 18
+ball.tileset_width = 18
+ball.tileset_height = 18
+ball.quad = love.graphics.newQuad(ball.x_tile_pos, ball.y_tile_pos, ball.tile_width, ball.tile_height, ball.tileset_width, ball.tileset_height)
 ball.speed_x = 200
 ball.speed_y = 200
-ball.radius = 10
+ball.radius = ball.tile_width / 2
 ball.width = 2 * ball.radius
 ball.height = 2 * ball.radius
 --object functions
@@ -14,6 +22,7 @@ function ball.update(dt)
 	ball.position_y = ball.position_y + ball.speed_y * dt
 end
 function ball.draw()
+	love.graphics.draw(ball.image, ball.quad, ball.position_x - ball.radius, ball.position_y - ball.radius)
 	local segments_in_circle = 16
 	love.graphics.circle('line', ball.position_x, ball.position_y, ball.radius, segments_in_circle )
 end
--- src/platform.lua
+++ src/platform.lua
@@ -3,8 +3,16 @@ local platform = {}
 --object properties
 platform.position_x = 500
 platform.position_y = 500
+platform.image = love.graphics.newImage("img/platform.png")
+platform.x_tile_pos = 0
+platform.y_tile_pos = 0
+platform.tile_width = 85
+platform.tile_height = 20
+platform.tileset_width = 370
+platform.tileset_height = 90
+platform.quad = love.graphics.newQuad(platform.x_tile_pos, platform.y_tile_pos, platform.tile_width, platform.tile_height, platform.tileset_width, platform.tileset_height)
 platform.speed_x = 300
-platform.width = 70
+platform.width = 85
 platform.height = 20
 --object functions
 function platform.update(dt)
@@ -17,7 +25,8 @@ function platform.update(dt)
 	end
 end
 function platform.draw()
-	love.graphics.rectangle('fill', platform.position_x - platform.width / 2, platform.position_y - platform.height / 2, platform.width, platform.height)
+	love.graphics.draw(platform.image, platform.quad, platform.position_x - platform.width / 2, platform.position_y - platform.height / 2)
+	--love.graphics.rectangle('line', platform.position_x - platform.width / 2, platform.position_y - platform.height / 2, platform.width, platform.height)
 end
 function platform.stop(x,y)
 	platform.position_x = platform.position_x + x
